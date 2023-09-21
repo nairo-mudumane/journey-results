7832f97e9d2aeb23ee642ad73735da00faadc13d
nairo-mudumane <nairomudumane@outlook.com>
2023-09-21 14:50:02 +0200

    feat: tailwind theme

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ["./index.html", "./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#242424",
+        primary: "#4CAF50",
+        secondary: "#FFC107",
+        tertiary: "#FF5722",
+        shadowDark: "#333333",
+        shadowLight: "#009688",
+      },
+    },
+  },
+  plugins: [],
+};
